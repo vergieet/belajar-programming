7a059d5e86cc9888548a330b49154c2c9cfb90a4
Vergie Ericson T <vergieericsont@gmail.com>
2022-11-01 07:33:07 +0700

    Add more detailed explanation

--- 1-pelajaran-variabel-dan-tipe-data.php
+++ 1-pelajaran-variabel-dan-tipe-data.php
@@ -2,9 +2,9 @@
 // variabel = wadah untuk menyimpan nilai
 // cara penulisan variabel di php => $nama_variabel
 // tipe data = jenis nilai yang ditampung oleh variabel
-$namadepan = "Muhammad"; // string => text / tulisan
+$namadepan = "John"; // string => text / tulisan
 $manda = 10;
-$nama = "Muhammad"; // string => text / tulisan
+$nama = "Doe"; // string => text / tulisan
 $angka1 = 100; // integer => angka bulat
 $angka2 = 14.2; // float => angka desimal
 $sudahmengaji = false; // boolean => true / false
@@ -14,5 +14,3 @@ $angkapendaftaran = array(10, 2, 3,1,3,5); // array => berupa list / array
 echo "Angka 1 =" . $angka1 . "<br/>";
 echo "Angka 2 =" . $angka2. "<br/>";
 echo "Hasil penjumlahan = " . $angka1 + $angka2 ;
-//   10      .  14.2   . Muhammad;
-//   24.2Muhammad
